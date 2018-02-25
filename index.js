3d66691967126eab43cec32fe7974171e0102bc5
sergio4488 <36795715+sergio4488@users.noreply.github.com>
2018-02-25 20:11:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
     console.log("Bot is ready! ${bot.user.username}");
 
-    try {
-        let link = await bot.generatorInvite(["ADMINISTRATOR"]);
-        console.log(link);
-    } catch(e) {
-        console.log(e.stack);
-    }
 });
 
 bot.login(process.env.BOT_TOKEN);
