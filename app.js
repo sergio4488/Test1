cf02b784f2e6f3881c03518fde3c6a3e889b5d5e
sergio4488 <36795715+sergio4488@users.noreply.github.com>
2018-02-25 17:29:21 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: True});
+
+bot.on("ready", async () => {
+    console.log("Bot is ready! ${bot.user.username}");
+
+    try {
+        let link = await bot.generatorInvite(["ADMINISTRATOR"]);
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);
+    }
+});
+
+bot.login(BOT_TOKEN);
