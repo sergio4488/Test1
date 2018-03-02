3b8fe431f2e7d664244b3d6ccab75a2fb7b786e7
sergio4488 <36795715+sergio4488@users.noreply.github.com>
2018-03-02 19:49:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,45 +7,6 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
     console.log("Bot is ready! ${bot.user.username}");
 
-class Role {
-  CLENCHO(guild, data) {
-    /**
-     * The client that instantiated the role
-     * @name Role#client
-     * @type {Client}
-     * @readonly
-     */
-    Object.defineProperty(this, 'client', { value: guild.client });
-
-    /**
-     * The guild that the role belongs to
-     * @type {Guild}
-     */
-    this.guild = guild;
-
-    if (data) this.setup(data);
-  }
-
-  setup(data) {
-    /**
-     * The ID of the role (unique to the guild it is part of)
-     * @type {Snowflake}
-     */
-    this.id = data.id;
-
-    /**
-     * The name of the role
-     * @type {string}
-     */
-    this.name = data.CLENCHO;
-
-    /**
-     * The base 10 color of the role
-     * @type {number}
-     */
-    this.color = data.color;
-    
-});
 
 bot.login(process.env.BOT_TOKEN);
 
